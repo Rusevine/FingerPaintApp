d73ceb626e993095e000b42b3c7115e2cb71f0c0
Wiljay <wiljay.flores@gmail.com>
2018-08-10 13:50:06 -0400

    set up my UI layout

---
+++ FingerPaintApp/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  FingerPaintApp
+//
+//  Created by Wiljay Flores on 2018-08-10.
+//  Copyright © 2018 wiljay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
